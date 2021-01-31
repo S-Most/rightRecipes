2c879019682ae84159fb26ae75fb6ad8fa31ca10
HoornbloemCoding <71282824+HoornbloemCoding@users.noreply.github.com>
2021-01-31 17:01:02 +0100

    going home..

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,3 +1,49 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../../client";
+import "./blog.css";
+
 export default function Blog() {
-    return <div>Blog</div>;
+	const [postData, setPost] = useState(null);
+
+	useEffect(() => {
+		sanityClient
+			.fetch(
+				`*[_type == "post"]{
+                title,
+                slug,
+                mainImage{asset->{_id,url},alt}}`
+			)
+			.then((data) => setPost(data))
+			.catch(console.error);
+	}, []);
+
+	return (
+		<main>
+			<section>
+				<h1>Blog Posts Page</h1>
+				<h2>What subject do you fancy?</h2>
+				<div>
+					{postData &&
+						postData.map((post, index) => (
+							<article key={index}>
+								<Link
+									to={"/blog/" + post.slug.current}
+									key={post.slug.current}
+								>
+									<div>
+										<h3>{post.title}</h3>
+										<img
+											class='clear'
+											src={post.mainImage.asset.url}
+											alt=''
+										/>
+									</div>
+								</Link>
+							</article>
+						))}
+				</div>
+			</section>
+		</main>
+	);
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,13 @@
-import "./home.css"
-import Background from "../../img/_homeBackground.jpg"
+import "./home.css";
+import Background from "../../img/_homeBackground.jpg";
 
 export default function Home() {
-    return (
-        <main>
-            <img src={Background} alt=""/>
-            <section>
-                <h1>Choose your Recipe!</h1>
-            </section>
-        </main>
-    )
-    ;
+	return (
+		<main>
+			<img class='background' src={Background} alt='' />
+			<section>
+				<h1>Choose your Recipe!</h1>
+			</section>
+		</main>
+	);
 }

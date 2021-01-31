1b8f643bd43d11a4e9645930594522f0236b5259
HoornbloemCoding <71282824+HoornbloemCoding@users.noreply.github.com>
2021-01-31 13:31:10 +0100

    cleanup and start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from "./components/Home"
+import Recipes from "./components/Recipes"
+import SingleRecipe from "./components/SingleRecipe"
+import Blog from "./components/Blog"
+
 function App() {
     return (
-        <div className="App">
-            <h1>Test</h1>
-        </div>
+      <BrowserRouter>
+        <Switch>
+          <Route component={Home} path='/' exact />
+          <Route component={Recipes} path='/recipes' />
+          <Route component={SingleRecipe} path='/recipe/:slug' />
+          <Route component={Blog} path='/blog' />
+        </Switch>
+      </BrowserRouter>
+
     );
 }
 
